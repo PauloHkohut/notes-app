597ee108b0f830f15f4d627dc5d27ac074e59dc6
Paulo Henrique <pauloq_henrique@hotmail.com>
2021-06-01 20:42:43 -0300

    Att problems

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,6 +34,7 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
+    '@nuxtjs/pwa'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
@@ -66,5 +67,22 @@ export default {
         }
       }
     }
+  },
+
+  pwa: {
+    icon: {
+      source: "icon.png"
+    },
+    meta: {
+      theme_color: "#FCC344",
+      appleStatusBarStyle: "black-translucent"
+    },
+    manifest: {
+      name: "Notes App TADS",
+      short_name: "Notes App",
+      description: "Aplicativo para anotações do curso de TADS",
+      lang: "pt-br"
+    }
   }
-}
+
+};
