6b79c956270a5dd47469a68e885f9d67b8b733bd
Paulo Henrique <pauloq_henrique@hotmail.com>
2021-06-06 22:01:32 -0300

    Att

---
+++ components/TTaglist.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <ul class="list-unstyled">
+      <li v-for="(item, index) of value" :key="index">
+        <n-taglist-item v-model="value[index]"></n-taglist-item>
+      </li>
+      <li>
+        <n-taglist-item
+          v-model="tag"
+          @itemConfirmado="adicionar()"
+        ></n-taglist-item>
+      </li>
+    </ul>
+
+    <p class="text-muted">
+      <small>{{ mensagemNumeroItens }} </small>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "n-taglist",
+  props: {
+    value: Array
+  },
+  data() {
+    return {
+      tag: {
+        nome: null
+      }
+    };
+  },
+  computed: {
+    numeroItens() {
+      return this.value.length;
+    },
+    mensagemNumeroItens() {
+      return this.value.length > 1
+        ? `${this.numeroItens} itens adicionados`
+        : `${this.numeroItens} item adicionado`;
+    }
+  },
+  methods: {
+    adicionar() {
+      this.value.push(this.tag);
+
+      this.tag = {
+        nome: null
+      };
+    }
+  }
+};
+</script>
+
+<style></style>
--- static/OneSignalSDKWorker.js
+++ static/OneSignalSDKWorker.js
@@ -1,1 +1,1 @@
-importScripts('/sw.js?1622598239596', 'https://cdn.onesignal.com/sdks/OneSignalSDK.js')
+importScripts('/sw.js?1623025503136', 'https://cdn.onesignal.com/sdks/OneSignalSDK.js')
